add1026bf19557b61839ab849420d8c699f21950
mahle68 <mahle68@gmail.com>
2023-02-14 13:28:34 +0100

    imu data exploration

--- 00_data_download.R
+++ 00_data_download.R
@@ -22,3 +22,35 @@ acc = getMovebankNonLocationData(EHB_FN_id, sensorID = 2365683, animalName = "D3
 mag = getMovebankNonLocationData(EHB_FN_id, sensorID = 77740402, animalName = "D324_512", login = creds, removeDuplicatedTimestamps = T),
 quat = getMovebankNonLocationData(EHB_FN_id, sensorID = 819073350, animalName = "D324_512", login = creds, removeDuplicatedTimestamps = T)
 )
+
+#checks -------------------------------------------------------------
+
+#make sure sampling frequencies are consistent
+lapply(IMU, function(x) x %>%  dplyr::select(contains("sampling_frequency")) %>%  distinct()) # all should be 20. as in 20 Hz sampling frequency
+
+#look at burst duration. as length of the imu raw data/number of axes
+lapply(IMU, function(x){
+  x %>%  
+    dplyr::select(contains("_raw") | contains("axes") | contains("sampling_frequency")) %>%  
+    mutate(n_raw_values = sapply(strsplit(.[[1]], " "), length), #each row of raw values is a character string, with spaces in between the values.
+           n_axes =  nchar(.[[2]])) %>%  #the axis values are also a string. e.g "XYZ"
+    mutate(samples_per_axis = n_raw_values/n_axes,
+           burst_duration_sec = samples_per_axis/.[[3]]) %>% 
+    distinct(burst_duration_sec) #acc burs duration is 1.2 seconds. mag and quat are 0.5 seconds
+    #distinct(samples_per_axis) #acc = 24; mag = 15; quat = 20 
+}) 
+
+#Create one dataframe with separate columns for each axis of each sensor -------------------------------------------------------------
+
+lapply(IMU, function(x){
+  
+  sensor <- substr(x$sensor_type,1,3)[1]
+  
+    x %>% 
+    mutate(x = )
+  
+  
+})
+
+
+
