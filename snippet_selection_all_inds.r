6fb0952d1e828adf5d1d0d723345f3f35eff5e9e
mahle68 <mahle68@gmail.com>
2023-04-05 09:17:44 +0200

    snippets for all individuals

--- snippet_selection_all_inds.r
+++ snippet_selection_all_inds.r
@@ -19,6 +19,46 @@ setwd("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projec
 ## STEP 1: open data  ----------------------------------------------------------------
 
 #segmented GPS data from 02_GPS_segmentation
+seg_files <- list.files("/home/enourani/ownCloud/Work/Projects/HB_ontogeny_eobs/git_repository/R_files/GPS_seg_Apr23/classifiedData", full.names = T)
+
+#for each individual, pick 4 soaring and 4 gliding snippets
+snippet_ls <- lapply(seg_files, function(ind){
+  
+  load(ind) #later on change the original code to save this as a rds file
+  gps <- HRdf_smooth; rm(HRdf_smooth); gc(gc())
+  
+  #filter out non-migratory flight
+  gps_migr <- gps %>% 
+    filter(between(location_lat,5,60) & #filter out breeding and wintering season movements
+             flightClust_smooth3 != "linear soaring") %>%  #filter out linear soaring for now. some thermal soaring got in there. i need to modify martina's code for this sp
+    mutate(yday = yday(timestamp),
+           hr = hour(timestamp)) %>% 
+    mutate(flight_bout_id = paste(yday,hr, flightClust_smooth3, sep = "_"))
+  
+  #extract the longest flight bouts per flight type
+  long_bouts <- gps_migr %>% 
+    add_count(flight_bout_id) %>% 
+    group_by(flight_bout_id) %>% 
+    slice(1) %>% 
+    group_by(flightClust_smooth3) %>% 
+    arrange(desc(n), .by_group = TRUE) %>%
+    slice(1:5)  %>% #extract the five longest bouts of each group 
+    pull(flight_bout_id)
+  
+  #explore
+  gps_sf <- gps_migr %>% 
+    filter(flight_bout_id %in% long_bouts) %>% 
+    st_as_sf(coords = c("location_long", "location_lat"), crs = wgs) 
+    
+  #mapview(gps_sf, zcol = "flightClust_smooth3")
+  
+  gps_sf
+  
+})
+
+
+mapview(snippet_ls[[10]], zcol = "flightClust_smooth3")
+
 load("GPSsegmentation_Mar23/classifiedData/animal_D320_475_classifiedBursts_df.rdata") #HRdf_smooth
 ind1 <- HRdf_smooth
 load("GPSsegmentation_Mar23/classifiedData/animal_D324_512_classifiedBursts_df.rdata")
@@ -26,9 +66,7 @@ load("GPSsegmentation_Mar23/classifiedData/animal_D324_512_classifiedBursts_df.r
 gps <- ind1 %>% 
   bind_rows(HRdf_smooth)
 
-gps_sf <- gps %>% 
-  filter(location_lat <= 60) %>%  #filter out breeding season movements
-  st_as_sf(coords = c("location_long", "location_lat"), crs = wgs)
+
   
 #open acc data (from 01a_ACC_prep.R)
 acc_g <- readRDS("Pritish_collab_IMU/animal_8986_classifiedAcc.rds")
