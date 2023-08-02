6b2892160d1c7d2d6a59de0af6df6f6d74e90d0a
mahle68 <mahle68@gmail.com>
2023-08-02 02:45:48 +0200

    update

---
+++ S01_GPS_prep.R
@@ -0,0 +1,4 @@
+#This code filters honey buzzard tracks for sea-crossing sections. then segment flight types.
+#follows Martina's code for segmentation: https://github.com/kamransafi/GoldenEagles/blob/main/WP3_Soaring_Ontogeny/MS1_soaring_skills/script1_GPSsegmentation_goldenEagles_newMarch2023.R
+#Elham Nourani PhD.
+#Aug 2. 2023. Canberra, AU
