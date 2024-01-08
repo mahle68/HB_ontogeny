fcab8ef41115b510bf44c793fa940a5cd66a99bc
Elham <mahle68@gmail.com>
2024-01-08 09:32:44 +0100

    update

--- 04_stationary_bursts.r
+++ 04_stationary_bursts.r
@@ -41,41 +41,25 @@ gr_speed_vec_mag <- gr_speed  %>%
   }))
 
 
-#extract stationary bursts based on ground speed and calculate vector magnitude
-stationary <- gr_speed %>% 
-  filter(ground_speed_closest_gps == 0) %>%
-  mutate(vector_mag = map_chr(eobs_acceleration_g, ~{
-    acc_g <- strsplit(.x, " ") %>% unlist() %>% as.numeric()
-    ax1 <- acc_g[axis_1_i]
-    ax2 <- acc_g[axis_2_i]
-    ax3 <- acc_g[axis_3_i]
-    vector_mag <- vect_mag(ax1, ax2, ax3)
-    as.character(vector_mag) %>% str_c(collapse = " ")
-  }))
+#extract stationary bursts based on ground speed
+ stationary <- gr_speed %>% 
+   filter(ground_speed_closest_gps == 0) 
 
+#plot relationship between ground speed and accelerometry
+hist(str_split(stationary$vector_mag, " ") %>% unlist() %>% as.numeric(), xlab = "Vector magnitude", ylab = "Frequency", main = "")
 
 #plot the histogram of all vector magnitudes
 #2022
-stationary22 <- stationary %>% 
-  filter(year(timestamp) == 2022)
-hist(str_split(stationary22$vector_mag, " ") %>% unlist() %>% as.numeric())
+#stationary22 <- stationary %>% 
+#  filter(year(timestamp) == 2022)
+#hist(str_split(stationary22$vector_mag, " ") %>% unlist() %>% as.numeric())
 
 #2023
-stationary23 <- stationary %>% 
-  filter(year(timestamp) == 2023)
-hist(str_split(stationary23$vector_mag, " ") %>% unlist() %>% as.numeric())
-
-
-ggplot(df, aes(x=value)) +
-  geom_histogram(binwidth=1) +
-  facet_wrap(~year, scales="free_y") +
-  theme_minimal() +
-  xlab("Value") +
-  ylab("Count") +
-  ggtitle("Histogram per Year")
-
+#stationary23 <- stationary %>% 
+#  filter(year(timestamp) == 2023)
+#hist(str_split(stationary23$vector_mag, " ") %>% unlist() %>% as.numeric())
 
 
 #convert to sf object and plot on map
-stationary_sf <- stationary %>% 
-  st_as_sf(coords = c("location_long_closest_gps", "location_lat_closest_gps"), crs = wgs)
+#stationary_sf <- stationary %>% 
+# st_as_sf(coords = c("location_long_closest_gps", "location_lat_closest_gps"), crs = wgs)
