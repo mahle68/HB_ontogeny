6bb2e50b05cab497994e4f0edf33a5ce110e3209
Elham <mahle68@gmail.com>
2024-07-24 08:07:08 +0200

    code for calculating laterality index per burst added

--- MS2_laterality/L02_QUAT_exploration.r
+++ MS2_laterality/L02_QUAT_exploration.r
@@ -12,6 +12,9 @@ library(viridis)
 setwd("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/R_files/")
 wgs <- st_crs("+proj=longlat +datum=WGS84 +no_defs")
 
+#source the imu conversion functions
+source("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/HB_ontogeny/MS1_IMU_classification/00_imu_diy.r")
+
 birds_2023 <- c("D329_013", "D329_012", "D329_015", "D329_014", "D326_193", "D326_192")
 
 #open quat data-in original resolution: prepared in 01b_imu_processing.r
@@ -22,7 +25,6 @@ birds_2023 <- c("D329_013", "D329_012", "D329_015", "D329_014", "D326_193", "D32
 or_summaries_w_gps <- readRDS("matched_GPS_IMU/GPS_matched_or_w_summaries_8secIDs_Jul24.rds") %>% #this version also has the IDs assigned to 8-sec bursts.
   drop_na(individual_local_identifier)
 
-
 #-------------------------------------------------------------------------------------
 # STEP1: what range of angles represents circling flight?
 #-------------------------------------------------------------------------------------
@@ -32,8 +34,7 @@ or_summaries_w_gps <- readRDS("matched_GPS_IMU/GPS_matched_or_w_summaries_8secID
 smpl <- or_summaries_w_gps %>% 
   filter(individual_local_identifier == "D329_013" &
   #         imu_burst_id %in% c(2672, 2674, 2676, 2677, 2678)
-    imu_burst_id %in% c(4927:5817)
-  )
+    imu_burst_id %in% c(4927:5817))
 
 
 smpl_burst_sf <- smpl %>% 
@@ -112,7 +113,7 @@ circling_ys_no_sf <- data_circling %>%
 mapview(circling_ys_no_sf[1:5000,], zcol = "circling_ys_no")
 
 #-------------------------------------------------------------------------------------
-# STEP2: during circling, what is bank angle like?
+# STEP2: during circling, what is bank angle like? (using 8-sec summaries)
 #-------------------------------------------------------------------------------------
 #use cumulative roll for all individuals within circling bouts
 
@@ -153,7 +154,7 @@ X11(width = 10, height = 15)
             aes(x = start_location_long_closest_gps, 
                 y = start_location_lat_closest_gps, 
                 col = circling), size = 1.2)
-) 
+)
   
 ### check the circling assignments with the 1Hz GPS data for a subset ####
 #use smpl_burst_sf from above
@@ -256,37 +257,40 @@ mapview(short_day_sf) #mainly during the wintering OR the breeding season.
 
 
 #-------------------------------------------------------------------------------------
-# STEP3: Calculate degree of handedness for each day
+# STEP3: Calculate degree of handedness for each 8-second burst
 #-------------------------------------------------------------------------------------
 
 #open quat data- aggregated for every second, matched with gps-informed flight classification: prepared in 01b_imu_processing.r
 or_summaries_w_gps <- readRDS("matched_GPS_IMU/GPS_matched_or_w_summaries_8secIDs_Jul24.rds") %>% #this version also has the IDs assigned to 8-sec bursts.
   drop_na(individual_local_identifier)
 
-# #open the dataframe with one-second data. filter for circl
-# #---------------------- calculate laterality for each 8-second burst
-# #assign circling vs non-circling to values of yaw
-# laterality_8sec <- or_8sec_summaries_w_gps %>% #this has one row per 8-9-sec burst 
-#   mutate(burst_dur2 = difftime(end_timestamp, start_timestamp)) %>% 
-#   filter(burst_dur2 > 6.5 &  #remove bursts that are shorter than 6.5 seconds
-#            cumulative_yaw_8sec >= 45 | cumulative_yaw_8sec <= -45) %>%  #only keep thermalling flight. use a threshold of 45 degrees in 8 seconds.
-#   mutate(bank_direction = ifelse(cumulative_roll_8sec < 0, "left",
-#                                  ifelse(cumulative_roll_8sec > 0, "right", "straight")),
-#          heading_direction = ifelse(cumulative_yaw_8sec < 0, "left",
-#                                     ifelse(cumulative_yaw_8sec > 0, "right", "straight")),
-#          unique_date = as.character(date(start_timestamp)),
-#          yday = yday(start_timestamp)) %>% #this will make it easier to filter later for life cycle stages 
-#   group_by(individual_local_identifier, unique_date) %>% #group by individual ID and day
-#   summarise(n_bursts_in_day = n(),
-#             yday = head(yday, 1),
-#             bank_left = sum(bank_direction == "left"),
-#             bank_right = sum(bank_direction == "right"),
-#             bank_straight = sum(bank_direction == "straight"),
-#             heading_left = sum(heading_direction == "left"),
-#             heading_right = sum(heading_direction == "right"),
-#             heading_straight = sum(heading_direction == "straight"),
-#             laterality_bank = (bank_right - bank_left)/(bank_right + bank_left),
-#             laterality_heading = (heading_right - heading_left)/(heading_right + heading_left),
-#             .groups = 'drop') %>% 
-#   as.data.frame()
+#---------------------- calculate laterality for each 8-second burst
+#this is not filtered for circling/not OR the length of bursts
 
+laterality_1sec <- or_summaries_w_gps %>% #this has one row per seconds
+  mutate(bank_direction = ifelse(cumulative_roll < 0, "left",
+                                 ifelse(cumulative_roll > 0, "right", "straight")),
+         heading_direction = ifelse(cumulative_yaw < 0, "left",
+                                    ifelse(cumulative_yaw > 0, "right", "straight"))) %>% #this will make it easier to filter later for life cycle stages
+  group_by(burst_id_8sec) %>% #group by 8-sec burst IDs, which are unique across individuals
+  summarise(n_records = n(),
+            individual_local_identifier = head(individual_local_identifier, 1),
+            bank_left = sum(bank_direction == "left"),
+            bank_right = sum(bank_direction == "right"),
+            bank_straight = sum(bank_direction == "straight"),
+            heading_left = sum(heading_direction == "left"),
+            heading_right = sum(heading_direction == "right"),
+            heading_straight = sum(heading_direction == "straight"),
+            laterality_bank = (bank_right - bank_left)/(bank_right + bank_left),
+            laterality_heading = (heading_right - heading_left)/(heading_right + heading_left),
+            across(c(timestamp, timestamp_closest_gps, location_lat_closest_gps, location_long_closest_gps, height_above_ellipsoid_closest_gps, ground_speed_closest_gps, heading_closest_gps), 
+                   ~head(.,1),
+                   .names = "start_{.col}"),
+            across(c(flight_type_sm2, flight_type_sm3, track_flight_seg_id),
+                   ~Mode(.), #make sure the Mode function is defined
+                   .names = "{.col}_Mode"),
+            end_timestamp = tail(timestamp,1),
+            .groups = 'drop') %>%
+  as.data.frame()
+
+saveRDS(laterality_1sec, file = "laterality_index_per_8sec_burst.rds")
