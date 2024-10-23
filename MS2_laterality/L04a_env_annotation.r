c790fe36c6a2583511f6b60f8f9a9d919db34f97
Elham <mahle68@gmail.com>
2024-10-23 12:43:03 +0200

    update

--- MS2_laterality/L04a_env_annotation.r
+++ MS2_laterality/L04a_env_annotation.r
@@ -3,142 +3,178 @@
 #14.10.2024 Konstanz, Germany
 #Elham Nourani. enourani@ab.mpg.de
 
+library(tidyverse)
+library(lubridate)
+library(terra)
+library(sf)
+library(ncdf4)
+library(parallel)
+
+setwd("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/R_files/")
+
+
+#annotating data on movebank failed. even when smaller chunks of data were annotated and returned, many rows had NaN values.
+#Data download using CDS API also failed because I didn't manage to set up the new api key
+#So, data was downloaded from the CDS website, separately for each year
+
+#-----------------------------------------------------------------------------
+## Step 1: decide on the unique hours for which data should be retrieved #####
+#-----------------------------------------------------------------------------
 
 #open data filtered and matched with GPS in L04_full_workflow_r
 
-or_w_gps_df <- readRDS("thinned_laterality_w_gps.rds") %>% 
-  drop_na(location_long_closest_gps_raw) %>% 
-  mutate(row_id = row_number(),
-         #create grouping factor to split the df into 5 groups for annotation request based on latitude and time (each request can't be bigger than 10GB)
-         lat_group = case_when(
-           location_lat_closest_gps_raw >= 45 ~ "one",
-           between(location_lat_closest_gps_raw, 27, 45) ~ "two",
-           between(location_lat_closest_gps_raw, 9, 27) ~ "three",
-           between(location_lat_closest_gps_raw, -9, 9) ~ "four",
-           location_lat_closest_gps_raw <= -9 ~ "five"
-         ))
-#create grouping factor to split the df into 5 groups for annotation request (each request can't be bigger than 10GB)
-#annotation_group = c(rep(1:4, each = ceiling(nrow(or_w_gps_df)/5)), rep(5, nrow(or_w_gps_df) - ceiling(nrow(or_w_gps_df)/5)*4)))
+#or_w_gps_df <- readRDS("thinned_laterality_w_gps.rds") #2022-08-20 17:16:14.0000 to 2024-04-15 10:00:46.0000 
+
+all_gps_apr <- readRDS("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/data/all_gps_apr15_24.rds") %>%  #2022-09-25 07:56:41.0000 to 2024-04-15 10:02:33.0000
+  filter(!is.na(individual_local_identifier)) %>% 
+  mutate(yr = year(timestamp),
+         mn = month(timestamp),
+         dy = day(timestamp),
+         hr = hour(timestamp),
+         unique_hr = paste(yr,mn,dy,hr, sep = "_"),
+         closest_hr = round(timestamp, units="hours") %>% as.character())
+
+# unique_hrs <- all_gps_apr  %>% 
+#   group_by(closest_hr) %>% 
+#   slice(1) #7327 unique hours; 7311 closest hour
+
+all_gps_ls <- split(all_gps_apr, all_gps_apr$yr)
+
+#convert everything to SpatVector for ease of extraction later on. this was not a good idea. PC crashed when trying to split one year of data into a list of hours
+#all_gps_ls <- lapply(all_gps_ls, function(yr) vect(yr, geom = c("location_long", "location_lat"), crs =  "EPSG:4326"))
+
+#---------------------------------------------------
+## Step 2: extract wind u and v for each point #####
+#---------------------------------------------------
+
+#list nc files
+nc_files <- list.files("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/data/laterality_annotations/WIND_FROM_CDS",
+                       pattern = ".nc", full.names = T)
+
+lapply(all_gps_ls, function(x){
+  
+  #extract the year
+  yr <- x$yr[1]
+  
+  ########### read in wind data for this year #####
+  #open nc data for this year
+  u <- nc_files[grep(yr, nc_files)] %>% 
+    rast("u")
+  
+  v <- nc_files[grep(yr, nc_files)] %>% 
+    rast("v")
+  
+  #for some reason I can't extract time using the terra::time() function. Extract the time from the layer names
+  times <- names(u) %>%
+    str_split("time=") %>%
+    map_chr(2)
+  
+  #reference time according to the nc file
+  ref_date <- ymd_hms("1970-01-01 00:00:00")
+  
+  #convert the hours into date + hour
+  timestamp <- ref_date + seconds(times)
+  
+  #rename the layers based on the converted timestamps. these are the same for both u and v. but keep the u and v in the names for clarity when extracting values
+  #from the two layers that would otherwise have the same name (ie. the timestamp)
+  names(u) <- paste0("u_900_", timestamp)
+  names(v) <- paste0("v_900_", timestamp)
+  
+  
+  ########### split the tracking data into unique hours #####
+  x_ls <- split(x, x$closest_hr)
+  
+  # Define the number of cores to use
+  num_cores <- detectCores() - 4
+  
+  #for each hour
+  (st_time <- Sys.time())
+  wind_this_hr <- lapply(x_ls, function(y){
+    
+    #extract the unique hourc
+    unique_hr <- y$closest_hr[1]
+    
+    #extract the corresponding rasters
+    wind <- c(
+      u[[grep(unique_hr, names(u))]],
+      v[[grep(unique_hr, names(v))]]
+    )
+    #also could already annotate with wind speed. but having raw u and v could be helpful later for sure
+    #wind_speed <- sqrt(u[[grep(unique_hr, names(u))]]^2 + v[[grep(unique_hr, names(v))]]^2)
+    
+    if(nrow(wind) > 0 ){ #if there is matching env data for this hour, extract the wind data for each location.
+      
+      #convert tracking data to SpatVector
+      y <- vect(y, geom = c("location_long", "location_lat"), crs =  "EPSG:4326")
+      
+      #extract values for each point
+      y_df <- y %>% 
+        extract(x = wind, y = ., method = "bilinear", bind = T) %>% 
+        data.frame(., geom(.)) %>% 
+        dplyr::select(-c("geom", "part", "hole")) %>% 
+        rename_with(~ str_sub(.x, start = -25, end = -21), matches("u_900|v_900")) %>%  #remove timestamp from the column name
+        rename(location_lat = y,
+               location_long = x)
+      
+    } else { #if there are no matching wind data for this hour, still create y_df, but with NA values for u and v
+      
+      y_df <- y %>% 
+        mutate(u_900 = NA,
+               v_900 = NA)
+      
+    }
+    
+    return(y_df)
+    
+  }) %>% 
+    bind_rows()
+  
+  Sys.time() - st_time
+  
+  
+})
+#-------------------------------
+### or
+#extract manually using ncdf4
+
+#to extract meta-data
+nc_yr <- nc_open(nc_files[grep(yr, nc_files)])
+
+#extract lon and lat
+lat <- ncvar_get(nc_yr,'latitude')
+nlat <- dim(lat) 
+lon <- ncvar_get(nc_yr,'longitude')
+nlon <- dim(lon) 
+
+#extract the time
+t <- ncvar_get(nc_yr, "valid_time")
+nt <- dim(t)
+
+#reference time according to the nc file
+ref_date <- ymd_hms("1970-01-01 00:00:00")
+
+#convert the hours into date + hour
+timestamp <- ref_date + seconds(t)
+
+#put everything in a large df
+row_names <- expand.grid(lon, lat, timestamp)
+
+#extract data for variables
+var_df <- lapply(vname, function(i){
+  df <- data.frame(as.vector(ncvar_get(nc, i)))
+  colnames(df) <- paste0("data_", i)
+  df
+}) %>% 
+  bind_cols() %>% 
+  bind_cols(row_names)
+
+colnames(var_df) <- c(vname, "lon", "lat", "date_time") #set column names
 
 
 
-#try my luck with movebank #-----------------------------------------------------------------
-env_request <- or_w_gps_df %>% 
-  select(row_id, individual_local_identifier, location_long_closest_gps_raw, location_lat_closest_gps_raw, timestamp_closest_gps_raw, lat_group) %>% 
-  mutate(timestamp_closest_gps_raw = paste0(as.character(timestamp_closest_gps_raw), ".000")) %>% 
-  rename('location-long' = location_long_closest_gps_raw,
-         'location-lat' = location_lat_closest_gps_raw,
-         timestamp = timestamp_closest_gps_raw)
-
-#save each group separately as a csv
-env_request %>% 
-  group_by(lat_group) %>%
-  group_walk(~ write_csv(.x, paste0("laterality_annotation_group_", .y$lat_group, ".csv")))
-
-#groups 4 and 5 failed for wind annotations. Break them up further based on year (they are the only zones that each have data for 3 years)
-leftovers <- or_w_gps_df %>% 
-  filter(lat_group %in% c("four", "five")) %>% 
-  mutate(year = year(timestamp_closest_gps_raw),
-         new_groups = case_when(
-           lat_group == "four" & year == 2022 ~ "four_1",
-           lat_group == "four" & year == 2023 ~ "four_2",
-           lat_group == "four" & year == 2024 ~ "four_3",
-           lat_group == "five" & year == 2022 ~ "five_1",
-           lat_group == "five" & year == 2023 ~ "five_2",
-           lat_group == "five" & year == 2024 ~ "five_3",
-         ))  %>% 
-  select(row_id, individual_local_identifier, location_long_closest_gps_raw, location_lat_closest_gps_raw, timestamp_closest_gps_raw, new_groups, lat_group, year) %>% 
-  mutate(timestamp_closest_gps_raw = paste0(as.character(timestamp_closest_gps_raw), ".000")) %>% 
-  rename('location-long' = location_long_closest_gps_raw,
-         'location-lat' = location_lat_closest_gps_raw,
-         timestamp = timestamp_closest_gps_raw)
-
-leftovers %>% 
-  group_by(new_groups) %>%
-  group_walk(~ write_csv(.x, paste0("laterality_annotation_group_", .y$new_groups, ".csv")))
 
 
-#groups 4-1, 4-2, 5-1, 5-2 failed for wind annotations. Break them up further based on year
-
-leftovers2 <-  or_w_gps_df %>% 
-  mutate(year = year(timestamp_closest_gps_raw),
-         month = month(timestamp_closest_gps_raw)) %>% 
-  filter(lat_group %in% c("four", "five") & year %in% c(2022, 2023)) %>% 
-  mutate(new_groups = case_when(
-    lat_group == "four" & year == 2022 & month %in% c(1:6) ~ "four_1_1",
-    lat_group == "four" & year == 2022 & month %in% c(7:12) ~ "four_1_2",
-    lat_group == "four" & year == 2023 & month %in% c(1:6) ~ "four_2_1",
-    lat_group == "four" & year == 2023 & month %in% c(7:12) ~ "four_2_2",
-    lat_group == "five" & year == 2022 & month %in% c(1:6) ~ "five_1_1",
-    lat_group == "five" & year == 2022 & month %in% c(7:12) ~ "five_1_2",
-    lat_group == "five" & year == 2023 & month %in% c(1:6) ~ "five_2_1",
-    lat_group == "five" & year == 2023 & month %in% c(7:12) ~ "five_2_2"
-  ))  %>% 
-  select(row_id, individual_local_identifier, location_long_closest_gps_raw, location_lat_closest_gps_raw, timestamp_closest_gps_raw, new_groups, lat_group, year) %>% 
-  mutate(timestamp_closest_gps_raw = paste0(as.character(timestamp_closest_gps_raw), ".000")) %>% 
-  rename('location-long' = location_long_closest_gps_raw,
-         'location-lat' = location_lat_closest_gps_raw,
-         timestamp = timestamp_closest_gps_raw)
-
-
-leftovers2 %>% 
-  group_by(new_groups) %>%
-  group_walk(~ write_csv(.x, paste0("laterality_annotation_group_", .y$new_groups, ".csv")))
-
-
-
-# 4_1_2, 4_2_1, and 5_1_2, 5_2_1 failed
-leftovers3_rows <-  leftovers2 %>% 
-  filter(new_groups %in% c("four_2_1", "four_1_2", "five_1_2", "five_2_1"))
-
-leftovers3 <- or_w_gps_df %>% 
-  filter(row_id %in% leftovers3_rows$row_id) %>% 
-  mutate(year = year(timestamp_closest_gps_raw),
-         month = month(timestamp_closest_gps_raw),
-         hour = hour(timestamp_closest_gps_raw),
-         new_lat_groups = case_when(
-           location_lat_closest_gps_raw >= 5.5 ~ "new_4",
-           between(location_lat_closest_gps_raw, 1.5, 5.5) ~ "new_5",
-           between(location_lat_closest_gps_raw, -2, 1.5) ~ "new_6",
-           between(location_lat_closest_gps_raw, -5.5, -2) ~ "new_7",
-           between(location_lat_closest_gps_raw, -9, -5.5) ~ "new_8",
-           between(location_lat_closest_gps_raw, -12.5, -9) ~ "new_9",
-           between(location_lat_closest_gps_raw, -16, -12.5) ~ "new_10",
-           between(location_lat_closest_gps_raw, -19.5, -16) ~ "new_11",
-           between(location_lat_closest_gps_raw, -23, -19.5) ~ "new_12",
-           location_lat_closest_gps_raw <= -23 ~ "new_13")) %>% 
-  #long_group = ifelse(location_long_closest_gps_raw >= 15, 2, 1)) %>% #also use longitude to group the rows :/
-  mutate(new_groups = paste0(year, "_", new_lat_groups)) %>% 
-  select(row_id, individual_local_identifier, location_long_closest_gps_raw, location_lat_closest_gps_raw, timestamp_closest_gps_raw, new_groups, new_lat_groups, year, month, hour) %>% 
-  mutate(timestamp_closest_gps_raw = paste0(as.character(timestamp_closest_gps_raw), ".000")) %>% 
-  rename('location-long' = location_long_closest_gps_raw,
-         'location-lat' = location_lat_closest_gps_raw,
-         timestamp = timestamp_closest_gps_raw)
-
-leftovers3 %>% 
-  group_by(new_groups) %>%
-  group_walk(~ write_csv(.x, paste0("laterality_annotation_group_", .y$new_groups, ".csv")))
-
-
-
-# append annotated data to original dataset  
-
-#read in all annotation files for wind............... there are many NAN values!!!!!!!!
-
-wind_data <- list.files("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/data/laterality_annotations/wind", pattern = ".csv", full.names = T) %>% 
-  map(read.csv)
-
-
-#download directly from cds #-----------------------------------------------------------------
-
-#specify time periods for which data should be downloaded: not all years have data for all months, so specify this and submit the requests separately
-hours <- unique(hour(or_w_gps_df$timestamp_closest_gps_raw)) %>% str_pad(2,"left","0")
-yr_mn <- data.frame(yr = c(rep(2022, 5), rep(2023, 12), rep(2024, 4)),
-                    mnth = c(8:12, 1:12, 1:4) %>% str_pad(2,"left","0"))  
-days <- days <- c(1:31) %>% str_pad(2,"left","0")
-
-# 
-# 
+#----------------------------------------------------- 
 # ## cant set up the API. downloading from the gui
 # import cdsapi
 # 
