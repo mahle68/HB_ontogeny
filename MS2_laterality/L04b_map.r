22a564ce23ba6bb260c156260e3d076b3264da37
Elham <mahle68@gmail.com>
2024-12-17 09:58:16 +0100

    update

--- MS2_laterality/L04b_map.r
+++ MS2_laterality/L04b_map.r
@@ -9,8 +9,14 @@ library(ggridges)
 
 setwd("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/R_files/")
 
-# STEP 1: map tracking points ----------------------------------
+lat_zones <- seq(-30,65, by = 5)
 
+#--------------------------------------------------------------------
+# STEP 1: map tracking points ----------------------------------
+#--------------------------------------------------------------------
+
+
+# data prep
 #open metadata to extract deployment date (the tracking data has some undeployed points in it)
 deployment <- read.csv("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/data/EHB_metadata - Sheet1.csv") %>% 
   filter(nest_country == "Finland") %>% 
@@ -55,6 +61,9 @@ cleaned_gps <- data %>%
 saveRDS(cleaned_gps, "cleaned_gps_for_laterality_map.rds")
 
 
+#plot
+cleaned_gps <- readRDS("cleaned_gps_for_laterality_map.rds")
+
 wgs <- st_crs("+proj=longlat +datum=WGS84 +no_defs")
 
 #open the continent boundaries layer
@@ -75,8 +84,6 @@ pol <- st_polygon(
 ) %>% 
   st_sfc(crs = wgs)
 
-lat_zones <- seq(-30,65, by = 5)
-
 lat_zones_for_map <- lat_zones[-20]
 
 #make the flyway map
@@ -89,7 +96,7 @@ x11(height = 5.5, width = 3)
                  linetype = "dashed", color = "gray75", linewidth = 0.5) +
     # Annotate the lat_zones
     annotate("text", x = rep(-16.9, length(lat_zones_for_map)), y = lat_zones_for_map + 1.5, #adjust the spacing based on the final plot size
-             label = paste0(lat_zones_for_map, "°"), size = 2.7, hjust = 0, color = "gray75", fontface = "italic") +
+             label = paste0(lat_zones_for_map, "°"), size = 2.3, hjust = 0, color = "gray75", fontface = "italic") +
     geom_path(data = subset(cleaned_gps, life_stage == "migration"), 
               aes(x = location_long, y = location_lat, 
                   group = individual_local_identifier), 
@@ -102,15 +109,20 @@ x11(height = 5.5, width = 3)
               aes(x = location_long, y = location_lat, 
                   group = individual_local_identifier), 
               linewidth = .4, lineend = "round", linetype = "dotted") +
+    ggtitle("  Hourly locations") +
     xlim(-17, 42.5) +
     ylim(-35, 65) +
-    theme_void()
+    theme_void() +
+    theme(text = element_text(size = 9))
 )
 
-# STEP 2 whole trajectory: laterality ----------------------------------
+ggsave(plot = flyway_map, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/flyway_map.png", 
+       device = "png", width = 3, height = 5.5)
 
-lat_zones <- seq(-30,65, by = 10)
-lat_zones <- seq(-30,65, by = 5)
+
+#--------------------------------------------------------------------
+# STEP 2 whole trajectory: laterality ----------------------------------
+#--------------------------------------------------------------------
 
 #open laterality data (prepperd in L04_full_workflow.r). Same as the one used in the daily distribution plots
 #add latitudinal zone category
@@ -129,11 +141,12 @@ x11(height = 5.5, width = 2.3)
     geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
     geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
     geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
-    annotate("text", x = -90, y = 20.5, label = "Bank angle (°)", size = 3.7, hjust = 0, color = "black") +
+    #annotate("text", x = -90, y = 20.1, label = "Bank angle (°)", size = 3.5, hjust = 0, color = "black") +
+    ggtitle("Bank angle (°)") +
     xlim(-90, 90) +
-    scale_y_discrete(expand = expansion(add = c(1.5, 2))) +  # Add space above and below. to match the lat zones of the map
+    scale_y_discrete(expand = expansion(add = c(1.7, 1.5))) +  # Add space above and below. to match the lat zones of the map... place the violins in between the two lat lines
     theme_void() +
-    theme(plot.margin = unit(c(.7, .5, .98, .5), "lines"),
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
           text = element_text(size = 9),
           axis.line.x = element_line(), 
           axis.ticks.x = element_line(), 
@@ -142,14 +155,16 @@ x11(height = 5.5, width = 2.3)
     )
 )
 
-##see how the two plots look together
-X11(width = 5.5, height = 6)
-model_output_p <- grid.arrange(flyway_map, bank, nrow = 1, widths = c(0.5, 0.5))
+ggsave(plot = bank, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/laterality_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
 
 
+
+#--------------------------------------------------------------------
 # STEP 3 whole trajectory: hourly wind speed ----------------------------------
+#--------------------------------------------------------------------
 
-lat_zones <- seq(-30,65, by = 5)
+#data prep
 life_cycle <- readRDS("updated_life_cycle_nov24.rds")
 
 #open wind speed annotated data from L04a_env_annotation.r
@@ -160,7 +175,7 @@ wind_speed <- readRDS("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwd
   full_join(life_cycle %>% select(individual_local_identifier, deployment_dt_utc, first_exploration, migration_start, migration_end), by = "individual_local_identifier") %>% 
   #remove data before deployment
   filter(timestamp >= deployment_dt_utc) %>% 
-  group_by(individual_local_identifier, location_lat, location_long, unique_hr) %>%  #unique hour has the date in it too 
+  group_by(individual_local_identifier, unique_hr) %>%  #unique hour has the date in it too 
   slice(1) %>%  #make sure there is one value for each hour for each location for each individual
   ungroup() %>% 
   mutate(lat_zone = cut(location_lat,
@@ -172,18 +187,24 @@ wind_speed <- readRDS("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwd
 
 saveRDS(wind_speed, file = "wind_speed_for_the_map.rds")
 
+#plot
+
+wind_speed <- readRDS("wind_speed_for_the_map.rds")
+
 #violin pilot
 x11(height = 5.5, width = 2.3)
 (wsp <- ggplot(wind_speed, aes(x = wind_speed, y = lat_zone)) +
-    geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") +
-    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
+    #geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") + 
+    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) + 
     geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
     geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
-    annotate("text", x = -90, y = 20.5, label = as.character(expression("Hourly wind speed (m s"^-1*")")), 
-             size = 3.7, hjust = 0, color = "black") +
-    scale_y_discrete(expand = expansion(add = c(1.5, 2))) +  # Add space above and below. to match the lat zones of the map
+    #annotate("text", x = 0, y = 20.1, label = expression("Hourly wind speed (m s"^-1*")"), 
+    #         size = 3.5, hjust = 0, color = "black") +
+    scale_y_discrete(expand = expansion(add = c(1.7, 1.5))) +  # Add space above and below. to match the lat zones of the map
+    #ggtitle(expression("Hourly wind speed (m s"^-1*")")) + #the superscript will make it difficult to line up the panels
+    ggtitle("Hourly wind speed (m/s)") +
     theme_void() +
-    theme(plot.margin = unit(c(.7, .5, .98, .5), "lines"),
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
           text = element_text(size = 9),
           axis.line.x = element_line(), 
           axis.ticks.x = element_line(), 
@@ -193,9 +214,13 @@ x11(height = 5.5, width = 2.3)
 )
 
 
-# STEP 4 migration: Daily distance ----------------------------------
+ggsave(plot = wsp, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/wind_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
 
-lat_zones <- seq(-30,65, by = 5)
+
+#--------------------------------------------------------------------
+# STEP 4 migration: Daily distance ----------------------------------
+#--------------------------------------------------------------------
 
 #open migration data. prepared in L04_full_workflow......
 migr_daily <- readRDS("data_migration_performance_models_2min_daily.rds") %>% 
@@ -205,10 +230,42 @@ migr_daily <- readRDS("data_migration_performance_models_2min_daily.rds") %>%
                         right = FALSE)) %>%  # Include the left bin edge, exclude the right
   #remove data with no associated latitude
   drop_na(lat_zone)
+  
 
-# STEP 5 migration: hourly distance ----------------------------------
+#plot
 
-lat_zones <- seq(-30,65, by = 5)
+
+#violin pilot
+x11(height = 5.5, width = 2.3)
+(distance <- ggplot(migr_daily, aes(x = daily_distance, y = lat_zone)) +
+    geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") +
+    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
+    geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
+    #annotate("text", x = 0, y = 15, label = "Daily distance (km)", size = 3.5, hjust = 0, color = "black") +
+    ggtitle("Daily distance (km)") +
+    #xlim(-90, 90) +
+    scale_y_discrete(expand = expansion(add = c(8.5, 1.5))) +  # Add space above and below. to match the lat zones of the map... place the violins in between the two lat lines
+    theme_void() +
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
+          text = element_text(size = 9),
+          axis.line.x = element_line(), 
+          axis.ticks.x = element_line(), 
+          axis.text.x = element_text(margin = margin(t = 1)),
+          panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5) # Add this line
+    )
+)
+
+ggsave(plot = distance, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/distance_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
+
+
+
+#--------------------------------------------------------------------
+# STEP 5 migration: vedba,  ----------------------------------
+#--------------------------------------------------------------------
+
+#data prep for all hourly variables
 
 #open migration data. prepared in L04_full_workflow......
 migr_hrly <- readRDS("data_migration_performance_models_2min_hrly.rds") %>% 
@@ -219,20 +276,81 @@ migr_hrly <- readRDS("data_migration_performance_models_2min_hrly.rds") %>%
   #remove data with no associated latitude
   drop_na(lat_zone)
 
-# STEP 6 migration: hourly flight height ----------------------------------
+
+#plot
+
+#vedba
+x11(height = 5.5, width = 2.3)
+(vedba <- ggplot(migr_hrly, aes(x = hrly_mean_vedba, y = lat_zone)) +
+    geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") +
+    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
+    geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
+    #annotate("text", x = 0, y = 15, label = "Daily distance (km)", size = 3.5, hjust = 0, color = "black") +
+    ggtitle("Hourly VeDBA (g)") +
+    #xlim(-90, 90) +
+    scale_y_discrete(expand = expansion(add = c(8.5, 1.5))) +  # Add space above and below. to match the lat zones of the map... place the violins in between the two lat lines
+    theme_void() +
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
+          text = element_text(size = 9),
+          axis.line.x = element_line(), 
+          axis.ticks.x = element_line(), 
+          axis.text.x = element_text(margin = margin(t = 1)),
+          panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5) # Add this line
+    )
+)
+
+ggsave(plot = vedba, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/vedba_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
 
 
+#cumulative yaw
+x11(height = 5.5, width = 2.3)
+(yaw <- ggplot(migr_hrly, aes(x = hrly_mean_cum_yaw, y = lat_zone)) +
+    geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") +
+    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
+    geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
+    #annotate("text", x = 0, y = 15, label = "Daily distance (km)", size = 3.5, hjust = 0, color = "black") +
+    ggtitle("Hourly cumulative yaw (°)") +
+    #xlim(-90, 90) +
+    scale_y_discrete(expand = expansion(add = c(8.5, 1.5))) +  # Add space above and below. to match the lat zones of the map... place the violins in between the two lat lines
+    theme_void() +
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
+          text = element_text(size = 9),
+          axis.line.x = element_line(), 
+          axis.ticks.x = element_line(), 
+          axis.text.x = element_text(margin = margin(t = 1)),
+          panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5) # Add this line
+    )
+)
 
-# STEP 7 migration: hourly VeDBA ----------------------------------
+ggsave(plot = yaw, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/yaw_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
 
 
+#pitch
+x11(height = 5.5, width = 2.3)
+(pitch <- ggplot(migr_hrly, aes(x = hrly_mean_mean_pitch, y = lat_zone)) +
+    geom_jitter(height = 0.2, width = 0, alpha = 0.1, size = 0.2, color = "black") +
+    geom_violin(trim = T, alpha = 0.7, color = "black", linewidth = 0.2) +
+    geom_boxplot(width = 0.1, outliers = F, color = "black", linewidth = 0.2, fill = "white")+
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray75", linewidth = 0.5) +
+    #annotate("text", x = 0, y = 15, label = "Daily distance (km)", size = 3.5, hjust = 0, color = "black") +
+    ggtitle("Hourly average pitch (°)") +
+    #xlim(-90, 90) +
+    scale_y_discrete(expand = expansion(add = c(8.5, 1.5))) +  # Add space above and below. to match the lat zones of the map... place the violins in between the two lat lines
+    theme_void() +
+    theme(plot.margin = unit(c(.1, .5, .4, .5), "lines"),
+          text = element_text(size = 9),
+          axis.line.x = element_line(), 
+          axis.ticks.x = element_line(), 
+          axis.text.x = element_text(margin = margin(t = 1)),
+          panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5) # Add this line
+    )
+)
 
+ggsave(plot = pitch, filename = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/paper_prep/MS2_laterality/figures/pitch_panel.png", 
+       device = "png", width = 2.3, height = 5.5)
 
-# STEP 8 migration: hourly cum_yaw and pitch ----------------------------------
-
-
-
-
-
-#subset to only keep one row per day
 
