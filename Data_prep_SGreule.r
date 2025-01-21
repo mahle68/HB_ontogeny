7ee52a2e081fbcc605fd89d80837d60904089956
Elham <mahle68@gmail.com>
2025-01-21 09:10:58 +0100

    update

--- Data_prep_SGreule.r
+++ Data_prep_SGreule.r
@@ -1,10 +1,10 @@
 #preparing data for the request by Soeren Greule (June 2024)
 #requirements: 
 #1-min resolution. 50 km buffer around breeding site. both Swiss and Finnish birds
-
 #14.6.2024 Konstanz, DE.
 #Elham Nourani
 
+#update Janurary 2025. keep the original resolution, filter for spatial extent as before
 
 library(tidyverse)
 library(move2)
@@ -18,19 +18,19 @@ setwd("/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projec
 
 #open Finnish data and subset for period of interest
 
-finn_1min <- readRDS("data/all_gps_apr15_24.rds") %>% 
+finn <- readRDS("data/all_gps_apr15_24.rds") %>% 
   drop_na("location_long") %>% 
   filter(location_lat > 59.5) %>% 
   #subsample for every minute
-  mutate(dt_1min = round_date(timestamp, "1 minute")) %>% 
-  group_by(individual_local_identifier, dt_1min) %>% 
-  slice(1) %>% 
-  ungroup() %>% 
+  # mutate(dt_1min = round_date(timestamp, "1 minute")) %>% 
+  # group_by(individual_local_identifier, dt_1min) %>% 
+  # slice(1) %>% 
+  # ungroup() %>% 
   select(c(individual_local_identifier, individual_taxon_canonical_name, eobs_horizontal_accuracy_estimate, eobs_speed_accuracy_estimate, eobs_temperature,
          gps_satellite_count, ground_speed, heading, height_above_ellipsoid,  location_lat, location_long, timestamp)) %>% 
   as.data.frame()
 
-fnn_sf <- finn_1min %>%  
+fnn_sf <- finn %>%  
   st_as_sf(coords = c("location_long", "location_lat"), crs = wgs)
 
 
@@ -40,29 +40,29 @@ creds <- movebank_store_credentials(username = "mahle68", rstudioapi::askForPass
 HB_id <- movebank_get_study_id("European Honey Buzzard Switzerland")
 
 
-sw_1min <- movebank_retrieve(study_id = 2235142320, sensor_type_id = "gps",   #download data for all individuals 
+sw <- movebank_retrieve(study_id = 2235142320, sensor_type_id = "gps",   #download data for all individuals 
                          entity_type = "event",  attributes = "all",
                          timestamp_end = as.POSIXct("2023-10-15 00:00:00")) %>% 
   drop_na("location_long") %>% 
   filter(between(location_lat, 45.7, 48.5)) %>% 
-  mutate(dt_1min = round_date(timestamp, "1 minute")) %>% 
-  group_by(individual_local_identifier, dt_1min) %>% 
-  slice(1) %>% 
-  ungroup() %>% 
+  # mutate(dt_1min = round_date(timestamp, "1 minute")) %>% 
+  # group_by(individual_local_identifier, dt_1min) %>% 
+  # slice(1) %>% 
+  # ungroup() %>% 
   select(c(individual_local_identifier, individual_taxon_canonical_name, eobs_horizontal_accuracy_estimate, eobs_speed_accuracy_estimate, eobs_temperature,
            gps_satellite_count, ground_speed, heading, height_above_ellipsoid,  location_lat, location_long, timestamp)) %>% 
   as.data.frame()
 
 
-sw_sf <- sw_1min %>%  
+sw_sf <- sw %>%  
   st_as_sf(coords = c("location_long", "location_lat"), crs = wgs)
 
 
 
 #folder to write the file:
-data <- finn_1min %>% 
-  bind_rows(sw_1min) %>% 
+data <- finn %>% 
+  bind_rows(sw) %>% 
   as.data.frame()
 
-write.csv(data, file = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/collabs/Soeren_greule_2024/honey_buzzards_ch_fn.csv",
+write.csv(data, file = "/home/enourani/ownCloud - enourani@ab.mpg.de@owncloud.gwdg.de/Work/Projects/HB_ontogeny_eobs/collabs/Soeren_greule_2024/original_res/honey_buzzards_ch_fn_og_res.csv",
           row.names = F)
